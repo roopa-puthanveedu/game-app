2af068b8f5f8c0fcf6867fa8dd9bbf4c369dcacd
Roopa <roopa@qburst.com>
2023-04-19 22:08:35 +0530

    updated rawg api key

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "c7b18323a47d40c394ea5b019646b1f5",
+    key: "e429bd85efa54c35a1fb91dfc0b0a36f",
   },
 });
